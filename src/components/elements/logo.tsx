086d5b6efe303b10e1f70caaa3dff337a1d8e403
pcltuan7103 <tuanpcl7103@gmail.com>
2025-06-15 21:57:52 +0700

    [FEATURE]: Create logo

--- src/components/elements/logo.tsx
+++ src/components/elements/logo.tsx
@@ -12,14 +12,14 @@ const Logo = ({
     return (
         <h2
             className={cn(
-                "text-2xl text-shop-dark_blue font-black tracking-wider uppercase hover:text-shop-light_blue hoverEffect group font-sans",
+                "text-2xl text-shop-blue_100 font-black tracking-wider uppercase hover:text-shop-blue_50 hoverEffect group font-sans",
                 className,
             )}
         >
             TSho
             <span
                 className={cn(
-                    "text-shop-light_blue group-hover:text-shop-dark_blue hoverEffect",
+                    "text-shop-blue_50 group-hover:text-shop-blue_100 hoverEffect",
                     spanDesign,
                 )}
             >
